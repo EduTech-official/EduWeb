3fdc9c2c77c341feb7774c7bf421aefd936d5f90
Shreshth Agarwal <shreshth.agarwal12345@gmail.com>
2024-02-24 23:41:13 +0530

    Added Images in the Gallery page

--- src/Pages/Gallery2.jsx
+++ src/Pages/Gallery2.jsx
@@ -1,24 +1,31 @@
 const Gallery2 = () => {
   const images = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg",
+    "https://drive.google.com/thumbnail?id=1UHL6dYvigUKtr4pwyBWiB_YQS5g7mhqN&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1QTLq6APpQKqRuX1lKqTArpmldtrK9N3e&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1kCAvvydtPTTnAk1_l38gTAD7FMczxlvk&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1TbeM1px_v8fq0Eh4n3GMyR7KpN49gdSy&sz=w1000",
+
+    "https://drive.google.com/thumbnail?id=1b5q6F8lTz-nlBLomlDPchEW_xtncTCyH&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1h0gH1hn0iR8QC5wem6uV9ckLlMLcvt7D&sz=w1000",
+    "https://drive.google.com/thumbnail?id=160dFH4saFKdZjaZvVOE7Vky4qUDptmrj&sz=w1000",
+    "https://drive.google.com/thumbnail?id=129khytxqagQry1l3RK3945OTEneBnflj&sz=w1000",
+
+    "https://drive.google.com/thumbnail?id=18xuZpntRe1cWPQzpziJu2w9xZZvZ1Aq-&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1lS82tLa5ku5BQIgVBp7ZujHGWqqQvW0O&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1U4kRgvX6W-6SWoxUwLVLCs9mrn3Sr43f&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1C1JdB6_HnPPJfoqp6YiCY-CHqHY4edFU&sz=w1000",
+
+    "https://drive.google.com/thumbnail?id=1stuS_9uopMtKPhLqav06z1HQUHLODxnw&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1Eme3oVHSsvD_8jsoS0BPCeSyfm4cZ4eG&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1Cc1LJGfgA7dLE2Rg-edRK_CCFuViS9zJ&sz=w1000",
+    "https://drive.google.com/thumbnail?id=1co9BtUpL8zMwUXcO3LBND3mf3dMDPBu4&sz=w1000",
   ];
 
   return (
     <div>
       <div className="pt-8 grid grid-cols-2 md:grid-cols-4 justify-center gap-4 px-4">
         <div className="grid gap-4 justify-center">
-          {images.slice(0, 3).map((image, index) => (
+          {images.slice(0, 4).map((image, index) => (
             <div key={index}>
               <img
                 className="h-auto max-w-full rounded-lg"
@@ -29,7 +36,7 @@ const Gallery2 = () => {
           ))}
         </div>
         <div className="grid gap-4">
-          {images.slice(3, 6).map((image, index) => (
+          {images.slice(4, 8).map((image, index) => (
             <div key={index}>
               <img
                 className="h-auto max-w-full rounded-lg"
@@ -40,7 +47,7 @@ const Gallery2 = () => {
           ))}
         </div>
         <div className="grid gap-4 justify-center">
-          {images.slice(6, 9).map((image, index) => (
+          {images.slice(8, 12).map((image, index) => (
             <div key={index}>
               <img
                 className="h-auto max-w-full rounded-lg"
@@ -51,7 +58,7 @@ const Gallery2 = () => {
           ))}
         </div>
         <div className="grid gap-4">
-          {images.slice(9, 12).map((image, index) => (
+          {images.slice(12, 16).map((image, index) => (
             <div key={index}>
               <img
                 className="h-auto max-w-full rounded-lg"
