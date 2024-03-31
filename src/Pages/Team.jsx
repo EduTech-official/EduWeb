7b823a169def3bd7e0d93aef54c3ebc9e89b1bcf
Advait Jain <aj.advaitj@gmail.com>
2024-03-31 23:08:05 +0530

    FEAT: Add new dependencies and update styling in Gallery2, ContactPage, Team, and InfoCard components

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -8,11 +8,11 @@ const Team = () => {
     <div className="m-2 minmd:m-0 ">
       <div className=" lg:mx-2 minlg:mx-3">
         <div className="col-span-3 text-left h-[80vh] my-10">
-          <h2 className="text-md text-left text-white">DREAM</h2>
-          <h1 className="text-6xl minmd:text-xl font-semibold font-IBMplex text-left tracking-widest bg-gradient-to-r  from-[#E61AA1]  to-[#0CF996] to-10% bg-clip-text text-transparent mb-3 mr-auto">
+          <h2 className="text-md minmd:text-md text-left text-white">DREAM</h2>
+          <h1 className="text-6xl minmd:text-3xl font-semibold font-IBMplex text-left tracking-widest bg-gradient-to-r  from-[#E61AA1]  to-[#0CF996] to-10% bg-clip-text text-transparent mb-3 mr-auto">
             TEAM
           </h1>
-          <p className="text-stone-500 w-96 text-clamp font-IBMplex lg:p-4 minmd:w-fit mr-4 overflow-x-auto">
+          <p className="text-stone-500 w-96 font-IBMplex lg:p-4 minmd:w-fit text-xs mr-4 overflow-x-auto">
             Introducing our EduMinerva team: a visionary leader as President,
             highly skilled heads, and hardworking executives creating a
             development-minded environment. The President, highly goal-oriented,
@@ -26,7 +26,7 @@ const Team = () => {
         </div>
         {/* <div className="col-span-2x"></div> */}
       </div>
-      <div className="field m-auto -translate-y-[6rem] minlg:-translate-y-14">
+      <div className="field m-auto -translate-y-[6rem] minlg:-translate-y-24">
         {/* <div class="mouse"></div> */}
         <a href="#Team">
           <div className="scroll"></div>
