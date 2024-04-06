c3f5823c82a7f8424b6de36990a4467997b11693
Advait Jain <aj.advaitj@gmail.com>
2024-04-07 00:10:42 +0530

    Update ErrorPage styling

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -15,7 +15,7 @@ export default function ErrorPage() {
   }, []);
 
   return (
-    <div id="error-page" className="m-5">
+    <div id="error-page" className="m-5 text-white">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
