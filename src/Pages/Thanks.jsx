7310ba4e66a30a1217d75d42bd73050a6e2e2fbc
Shivank <shivank.agg.sa@gmail.com>
2024-10-20 23:15:02 +0530

    Added Whatsapp group link in the thanks page and also changed name of one team members

--- src/Pages/Thanks.jsx
+++ src/Pages/Thanks.jsx
@@ -3,14 +3,20 @@
 const Thanks = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold text-center animate-bounce text-white font-tourney">
-          Thank you for Registering!
-        </h1>
-        <p className="text-center text-white font-tourney">
-         You will have a lot of fun
-        </p>
-      </div>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-center animate-bounce text-white font-tourney">
+                Thank you for Registering!
+            </h1>
+            <p className="text-center text-white font-tourney">
+                You will have a lot of fun
+            </p>
+            <a
+                href="https://chat.whatsapp.com/H1mZ2n1Zqmb9mO9Acvwdkm"
+                className="mt-4 inline-block text-lg font-semibold text-green-500 hover:underline">
+                Join the WhatsApp Group
+            </a>
+
+        </div>
     </div>
   );
 };
