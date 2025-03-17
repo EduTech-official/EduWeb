049f76cfd237406f20e00f8e565eb56ac2d6a638
Shreshth Agarwal <68282910+shreshthagarwal@users.noreply.github.com>
2025-03-17 21:21:13 +0530

    Update TeamCards.jsx

--- src/components/TeamCards.jsx
+++ src/components/TeamCards.jsx
@@ -8,27 +8,25 @@ const TeamCards = ({ PicLink, Name, Position }) => {
   };
 
   return (
-    <div className="flex justify-center border-none" style={{ flex: "1 0 20%" }}>
+    <div className="flex justify-center items-center border-none" style={{ flex: "1 0 20%" }}>
       <div
         id="hove"
-        className="w-full h-full hover max-w-[16rem] min-w-[10rem] border-gray-200 rounded-xl bg-gray-800 bg-opacity-80 hover:bg-opacity-95 hover:brightness-110 dark:border-gray-700 p-4 flex-4 items-center"
+        className="w-full h-full max-w-[16rem] min-w-[10rem] border-gray-200 rounded-xl bg-gray-800 bg-opacity-80 hover:bg-opacity-95 hover:brightness-110 dark:border-gray-700 p-4 flex flex-col items-center text-center"
       >
         <img
           className="rounded-lg object-cover"
           src={PicLink ? PicLink : Logo}
-          alt="product image"
+          alt="team member"
           loading="lazy"
           style={{ height: 300, width: 300, objectFit: "cover" }}
         />
         <a href="#" onClick={handleClick}>
-          <h5 className="text-xl pt-3 text-left font-semibold tracking-tight text-white">
+          <h5 className="text-xl pt-3 font-semibold tracking-tight text-white">
             {Name}
           </h5>
         </a>
         <div className="my-3">
-          <span className="text-xs text-left font-bold text-white">
-            {Position}
-          </span>
+          <span className="text-xs font-bold text-white">{Position}</span>
         </div>
       </div>
     </div>
