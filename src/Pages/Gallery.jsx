37e7fa4a4e94bd019a0a4bf86c73c467bad88c3e
Harshit Sharma <kapilharshit470@gmail.com>
2025-09-01 20:10:15 +0530

    Updated gallery pages and added new images

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -21,6 +21,7 @@ const Gallery = () => {
           <div className="-m-1 flex flex-wrap md:-m-2">
             <div className="flex w-1/2 flex-wrap">
               <div className="w-1/2 p-1 md:p-2">
+              
                 <img
                   alt="gallery"
                   className="block h-full w-full rounded-lg object-cover object-center"
